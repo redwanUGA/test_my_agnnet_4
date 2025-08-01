350353122d03085386d932b105cffeff57dde771
Md. Redwan Islam <mi71296@uga.edu>
2025-08-01 09:12:52 -0400

    Run at least two epochs per experiment

--- run_all_experiments.sh
+++ run_all_experiments.sh
@@ -2,28 +2,30 @@
 # Run all model and dataset combinations sequentially.
 # Assumes datasets are located in the 'simple_data/' folder.
 
-python main.py --model BaselineGCN --dataset OGB-Arxiv --epochs 1
-python main.py --model BaselineGCN --dataset Reddit --epochs 1
-python main.py --model BaselineGCN --dataset TGB-Wiki --epochs 1
-python main.py --model BaselineGCN --dataset MOOC --epochs 1
+EPOCHS=2
 
-python main.py --model GraphSAGE --dataset OGB-Arxiv --epochs 1
-python main.py --model GraphSAGE --dataset Reddit --epochs 1
-python main.py --model GraphSAGE --dataset TGB-Wiki --epochs 1
-python main.py --model GraphSAGE --dataset MOOC --epochs 1
+python main.py --model BaselineGCN --dataset OGB-Arxiv --epochs $EPOCHS
+python main.py --model BaselineGCN --dataset Reddit --epochs $EPOCHS
+python main.py --model BaselineGCN --dataset TGB-Wiki --epochs $EPOCHS
+python main.py --model BaselineGCN --dataset MOOC --epochs $EPOCHS
 
-python main.py --model TGAT --dataset OGB-Arxiv --epochs 1
-python main.py --model TGAT --dataset Reddit --epochs 1
-python main.py --model TGAT --dataset TGB-Wiki --epochs 1
-python main.py --model TGAT --dataset MOOC --epochs 1
+python main.py --model GraphSAGE --dataset OGB-Arxiv --epochs $EPOCHS
+python main.py --model GraphSAGE --dataset Reddit --epochs $EPOCHS
+python main.py --model GraphSAGE --dataset TGB-Wiki --epochs $EPOCHS
+python main.py --model GraphSAGE --dataset MOOC --epochs $EPOCHS
 
-python main.py --model TGN --dataset OGB-Arxiv --epochs 1
-python main.py --model TGN --dataset Reddit --epochs 1
-python main.py --model TGN --dataset TGB-Wiki --epochs 1
-python main.py --model TGN --dataset MOOC --epochs 1
+python main.py --model TGAT --dataset OGB-Arxiv --epochs $EPOCHS
+python main.py --model TGAT --dataset Reddit --epochs $EPOCHS
+python main.py --model TGAT --dataset TGB-Wiki --epochs $EPOCHS
+python main.py --model TGAT --dataset MOOC --epochs $EPOCHS
 
-python main.py --model AGNNet --dataset OGB-Arxiv --epochs 1
-python main.py --model AGNNet --dataset Reddit --epochs 1
-python main.py --model AGNNet --dataset TGB-Wiki --epochs 1
-python main.py --model AGNNet --dataset MOOC --epochs 1
+python main.py --model TGN --dataset OGB-Arxiv --epochs $EPOCHS
+python main.py --model TGN --dataset Reddit --epochs $EPOCHS
+python main.py --model TGN --dataset TGB-Wiki --epochs $EPOCHS
+python main.py --model TGN --dataset MOOC --epochs $EPOCHS
+
+python main.py --model AGNNet --dataset OGB-Arxiv --epochs $EPOCHS
+python main.py --model AGNNet --dataset Reddit --epochs $EPOCHS
+python main.py --model AGNNet --dataset TGB-Wiki --epochs $EPOCHS
+python main.py --model AGNNet --dataset MOOC --epochs $EPOCHS
 
